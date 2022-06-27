80397945d1264d58398b71b88a3b88ab7af1f0ab
harikrishna <harikrishna6525@gmail.com>
2022-06-27 14:45:36 +0530

    :memo:calculator

--- Calculator/app.js
+++ Calculator/app.js
@@ -11,9 +11,35 @@ buttons.map(button => {
         console.log(e.target);
         const cal = e.target.innerText;
         // displayInput.append(cal);
-        displayInput.innerText += cal;
 
-    })
 
-})
+        switch (e.target.innerText) {
+            case 'C':
+                displayInput.innerText = '';
+                break;
+
+            case '←':
+
+                if (displayInput.innerText) {
+                    displayInput.innerText = displayInput.innerText.slice(0, -1);
+                }
+
+                break;
+
+            case '=':
+                try {
+                    displayInput.innerText = eval(displayInput.innerText);
+                }
+                catch {
+
+                    displayInput.innerText = 'Error!'
+                }
+                break;
+            default:
+                displayInput.innerText += cal;
+        }
+
+    });
+
+});
 
